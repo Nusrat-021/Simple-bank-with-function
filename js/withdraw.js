0eb183cb7d877c33e95b50b7fd3bbfbbfd4df569
Tanha-013 <nusrattanha897@gmail.com>
2023-08-22 16:39:52 +0600

    withdraw amount added through function

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,19 @@
-console.log('deposit');
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const newWithdrawAmount = getInputFieldById('withdraw-field');
+    // console.log(newWithdrawAmount);
+
+    const previousWithdrawTotal = getTextElementValueById('withdraw-total');
+    // console.log(previousWithdrawTotal);
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    setTextElementValueById('withdraw-total',currentWithdrawTotal);
+
+    const previousBalanceTotal = getTextElementValueById('total-balance');
+    // console.log(previousBalanceTotal);
+
+    const newBalanceFromWithdraw = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('total-balance', newBalanceFromWithdraw);
+
+})
+
